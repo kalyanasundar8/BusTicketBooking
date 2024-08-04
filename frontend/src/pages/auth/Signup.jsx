db3e6bc2e4db18b3aaba7514c9427c2c15e4556f
sundar8 <kalyanasundar8>
2024-08-04 11:49:08 -0400

    Signup page file added

---
+++ frontend/src/pages/auth/Signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='grid grid-cols-1 xl:grid-cols-2 mx-10 my-7'>
+      <main>
+        <h1 className='text-2xl font-bold'>BT</h1>
+        <div>
+
+        </div>
+      </main>
+      <main className='hidden xl:block bg-blue-600 h-[90vh] rounded-lg'>grid2</main>
+    </div>
+  )
+}
+
+export default Signup
